a326882111226cce4df8f086f2a9a8a47a25768a
Yelizaveta Semikina <lizasemikina42@gmail.com>
2026-03-29 20:02:57 -0500

    upload website

---
+++ documentation/js/custom.js
@@ -0,0 +1,23 @@
+(function ($) {
+ "use strict";
+$(document).ready(function(){
+	  
+		/*
+		Mean Menu Responsive
+		============================*/		
+        jQuery('nav#main-menu').meanmenu();				
+		/*
+		scrollUp
+		============================*/	
+		$.scrollUp({
+			scrollText: '^',
+			easingType: 'linear',
+			scrollSpeed: 900,
+			animation: 'fade'
+		});	
+
+		
+	});	
+
+})(jQuery);
+
